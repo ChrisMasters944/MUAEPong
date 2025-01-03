281b77067d0dda46639e85361b3d55a8ce19927f
ChrisMasters944 <133552146+ChrisMasters944@users.noreply.github.com>
2025-01-03 15:48:34 -0600

    Update pong-audio.js
    Fix soundtrack volume.

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -60,9 +60,9 @@ ambientSound.player.volume.value = -20; //turn down volume
 export var adventureMusic = new soundFile("soundtrack.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = -16;
+adventureMusic.player.volume.value = 0;
 
 export var villageMusic = new soundFile("soundtrack.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
-villageMusic.player.volume.value = -16;
+villageMusic.player.volume.value = 0;
